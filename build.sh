178bae3973a411d164d015e632d86374ae4efee4
Frederick Henderson <frederickjh@henderson-meier.org>
2020-07-27 11:21:10 +0200

    Fixes #2. This removes cpio that was missing on some systems. By using loops with cd and mkdir we can duplicate what cpio was doing with a simple pipe to create unknown directory structures to copy files into.

--- build.sh
+++ build.sh
@@ -22,15 +22,47 @@ do
   esac
 done
 
+function loadcolor(){
+# Colors  http://wiki.bash-hackers.org/snipplets/add_color_to_your_scripts
+# More info about colors in bash http://misc.flogisoft.com/bash/tip_colors_and_formatting
+esc_seq="\x1b["  #In Bash, the <Esc> character can be obtained with the following syntaxes:  \e  \033  \x1B
+NC=$esc_seq"39;49;00m" # NC = Normal Color
+# Colors with black background (40;)set for emails.
+red=$esc_seq"31;40;01m"
+green=$esc_seq"32;40;00m"
+yellow=$esc_seq"33;40;01m"
+blue=$esc_seq"34;40;01m"
+magenta=$esc_seq"35;40;01m"
+cyan=$esc_seq"36;40;01m"
+}
+loadcolor
+
+# Makes a directory if it is not found.
+function mkdirifnotfound(){
+  mkdir -p "$build_dir/$founddirectory"
+}
+
 # Clean build directory before building.
 rm -rf $build_dir
 mkdir -p "$build_dir/"
 
 # Download Fisher
-curl https://git.io/fisher --create-dirs -sLo config/fish/functions/fisher.fish
+curl https://git.io/fisher --create-dirs -sLo fish/functions/fisher.fish
 
 # Copy files to the build directory that will be uploaded to remote hosts.
-for f in pluginrc.fish config
+for filestocopy in pluginrc.fish fish
 do
-     find "$f" -depth -print | cpio --quiet -pd "$build_dir"
+  for found in $(find "$filestocopy" -depth -print)
+  do
+    if [ -f "$found" ]; then
+      founddirectory=$(dirname "$found")
+      if [ "$founddirectory" == "." ]; then
+        founddirectory=""
+      fi
+      mkdirifnotfound
+      cp "$found" "$build_dir/$founddirectory/"
+    else
+      mkdirifnotfound
+    fi
+  done
 done
--- pluginrc.fish
+++ pluginrc.fish
@@ -1,19 +1,40 @@
 #!/usr/bin/env fish
-set CURRENT_DIR (dirname (realpath (status current-filename)))
-set_color blue
+# Next line for testing.
+#set fish_trace true
 
+set_color blue
+set CURRENT_DIR (dirname (realpath (status current-filename)))
 cd $CURRENT_DIR
 
-if test -d config
-  mv config .config
+# Set the base directory that we want directories and files copied into on the remote server.
+# Normally this will be into $XDG_CONFIG_HOME which is set by default by xxh to:
+# $HOME/.xxh/.config
+set basedirectorytocopyfilesto $XDG_CONFIG_HOME
+
+# Makes a directory if it is not found.
+function mkdirifnotfound
+    mkdir -p "$basedirectorytocopyfilesto/$founddirectory"
 end
 
-diff "$XDG_CONFIG_HOME/fish/conf.d/fisher.fish" ".config/fish/conf.d/fisher.fish" &>/dev/null
+# Compare and check to see if the Fisher configuration files already exists or not and copy if not.
+diff "$basedirectorytocopyfilesto/fish/conf.d/fisher.fish" "fish/conf.d/fisher.fish" &>/dev/null
 set fisherfishdiff $status
 
-if test \( ! -e "$XDG_CONFIG_HOME/fish/functions/fisher.fish" \) -o \( ! $fisherfishdiff -eq 0 \)
-  set XXH_XDG_CONFIG_HOME (dirname (echo $XDG_CONFIG_HOME))
-  find ".config/" -depth -print | cpio --quiet -pd  "$XXH_XDG_CONFIG_HOME" &>/dev/null
+if test \( ! -e "$basedirectorytocopyfilesto/fish/functions/fisher.fish" \) -o \( ! $fisherfishdiff -eq 0 \)
+  # Set files then copy them. By default is this the files in the "fish" directory,
+  # that would normally be found in ~/.config/fish
+  set filestocopy "fish"
+  for found in (find "$filestocopy" -depth -print)
+      if test -f "$found"
+        set founddirectory (dirname "$found")
+        mkdirifnotfound
+        cp "$found" "$basedirectorytocopyfilesto/$founddirectory/"
+      else
+        mkdirifnotfound
+      end
+  end
 end
 
 set_color normal
+# For testing, ends trace.
+#set -e fish_trace
